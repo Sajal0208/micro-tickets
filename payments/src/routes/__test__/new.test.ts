2f433f8bc3412e8b8cf6d33969d2ab2826ceb56a
Sajal0208 <sajaldewangan@gmail.com>
2023-07-13 15:28:10 +0530

    added test scripts for orders, tickets and payments service

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -59,29 +59,3 @@ it('returns a 400 when purchasing a cancelled order', async () => {
         })
         .expect(400);
 })
-
-it('return a 204 with valid inputs', async () => {
-    const userId = new mongoose.Types.ObjectId().toHexString();
-    const order = Order.build({
-        id: new mongoose.Types.ObjectId().toHexString(),
-        userId,
-        version: 0,
-        price: 20,
-        status: OrderStatus.Created
-    });
-    await order.save();
-
-    await request(app)
-        .post('/api/payments')
-        .set('Cookie', global.signin(userId))
-        .send({
-            orderId: order.id,
-            token: 'tok_visa'
-        })
-        .expect(201);
-
-    const chargeOptions = (stripe.paymentIntents.create as jest.Mock).mock.calls[0][0];
-
-    expect(chargeOptions.amount).toEqual(20 * 100);
-    expect(chargeOptions.currency).toEqual('usd');
-})
